10fa93f6acc6533fe57cbb13078c33fec41f1188
Chalach.mo <driftkao@gmail.com>
2019-10-08 11:22:00 +0700

    edit script

--- scripts_sql/init-script.sql
+++ scripts_sql/init-script.sql
@@ -3,7 +3,8 @@ create table system_config(
 	id serial 	primary key not null,
 	"type" 		text not null,
 	"key" 		text not null,
-	"value" 	text not null
+	"value" 	text not null,
+	active 		int not null
 );
 
 create table master_role( 
@@ -28,30 +29,25 @@ create table map_role_menu(
 	sub_menu_id 	int not null
 );
 
-create table master_title(
-	id 			serial 	primary key not null,
-	"name" 		text not null
-);
 
 create table "user"( 
 	id 			serial 	primary key not null,
-	title 		int null,
-	"name" 		text not null,
-	surname 	text not null,
-	tel 		text ,
-	email 		text ,
-	"role" 		int not null 
+	pid 		text not null,
+	"role" 		int not null,
+	active 		int not null
 );
 
+
 INSERT INTO public.system_config
-("type","key","value")
-VALUES('WS','findAllCustomers', 'http://127.0.0.1/customers') ,
-('WS','findByIdCustomers', 'http://127.0.0.1/customers/:id') ,
-('DB','host', 'localhost') ,
-('DB','database', 'dashboard') ,
-('DB','user', 'postgres') ,
-('DB','password', 'admin') ,
-('DB','port', '5432');
+("type","key","value",active)
+VALUES('WS','findAllCustomers', 'http://127.0.0.1/customers',1) ,
+('WS','fake', 'https://jsonplaceholder.typicode.com/posts',1) ,
+('WS','fakeUnactive', 'https://jsonplaceholder.typicode.com/posts',0) ,
+('DB','host', 'localhost',0) ,
+('DB','database', 'dashboard',0) ,
+('DB','user', 'postgres',0) ,
+('DB','password', 'admin',0) ,
+('DB','port', '5432',0);
 
 INSERT INTO public.master_role
 ("name")
@@ -69,15 +65,14 @@ INSERT INTO public.map_role_menu
 (role_id,menu_id,sub_menu_id)
 VALUES(1,1,1) , (1,1,2) , (1,2,1) , (1,2,2), (2,1,1) , (2,1,2) , (3,2,1) , (3,2,2);
 
-INSERT INTO public.master_title
-("name")
-VALUES('นาย') , ('นาง'), ('นางสาว');
+--INSERT INTO public.master_title
+--("name")
+--VALUES('นาย') , ('นาง'), ('นางสาว');
 
 INSERT INTO public.user
-(title,"name",surname,tel,email,"role")
-VALUES (1,'สมชาย','นามสกุล','0801911150','somchai@thailife.com',1) ,
-(2,'สมศรี','นามสกุล','0801911150','somsree@thailife.com',2);
-
+(pid,"role",active)
+VALUES ('900-6468',1,1) ,
+('290-0582',2,1);
 
 
 -- FOR DEMO API
